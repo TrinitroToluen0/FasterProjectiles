32cd20042aa437b68ba626c0abcd5e8390f1cae3
TrinitroToluen0 <javiermenco404@gmail.com>
2024-04-06 14:19:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FasterProjectiles
+ A PocketMine-MP plugin to increase the speed of the projectiles
---
+++ src/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mencoreh\FasterProjectiles;
+
+use pocketmine\event\entity\ProjectileLaunchEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+    public bool $enabled;
+    public int $multiplier;
+
+
+    public function onEnable(): void {
+        $this->multiplier = $this->getConfig()->get("speed-multiplier");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+    }
+
+    public function onProjectileLaunch(ProjectileLaunchEvent $event): void {
+        if($event->isCancelled()) return;
+        
+        $entity = $event->getEntity();
+        $entity->setMotion($entity->getMotion()->multiply($this->multiplier));
+    }
+}
