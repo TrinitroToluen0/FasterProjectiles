2861da67713b436fdc41e1bb80815e511bcce1d5
TrinitroToluen0 <javiermenco404@gmail.com>
2024-04-15 23:12:59 -0500

    Testing how to change gravity

--- src/Main.php
+++ src/Main.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Mencoreh\FasterProjectiles;
 
-use pocketmine\entity\projectile\Projectile;
 use pocketmine\event\entity\ProjectileLaunchEvent;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\entity\EntityMotionEvent;
 
 
 class Main extends PluginBase implements Listener {
@@ -24,19 +22,9 @@ class Main extends PluginBase implements Listener {
 
     public function onProjectileLaunch(ProjectileLaunchEvent $event): void {
         if($event->isCancelled()) return;
-
         $entity = $event->getEntity();
+        $entity->setGravity($entity->getGravity() * $this->gravity);
         $entity->setMotion($entity->getMotion()->multiply($this->multiplier));
-    }
-
-    public function onEntityMotion(EntityMotionEvent $event): void
-    {
-        $entity = $event->getEntity();
-        if ($entity instanceof Projectile && !$event->isCancelled()) {
-            $motion = $entity->getMotion();
-            $motion->y -= $this->gravity;
-            $entity->setMotion($motion);
-        }
     }
 }
 
